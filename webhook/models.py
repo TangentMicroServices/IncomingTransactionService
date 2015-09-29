850f510596e8a585d15647679c0e7471304b085d
surfer190 <shutch190@gmail.com>
2015-09-29 12:22:47 +0200

    Make Source Field Choices

--- webhook/models.py
+++ webhook/models.py
@@ -3,7 +3,17 @@ from django.db import models
 class IncomingRequest(models.Model):
     last_edited = models.DateTimeField(auto_now=True,db_index=True)
     created = models.DateTimeField(auto_now_add=True,db_index=True)
-    source = models.CharField(blank=True, null=True, max_length=100)
+    IFTTT = 'IT'
+    HIPCHAT = 'HC'
+    UNKNOWN = 'UK'
+    SOURCE_CHOICES = (
+        (IFTTT, 'If This Then That'),
+        (HIPCHAT, 'Hipchat'),
+        (UNKNOWN, 'Unknown'),
+    )
+    source = models.CharField(max_length=2,
+                              choices=SOURCE_CHOICES,
+                              default=UNKNOWN)
     incoming_url = models.URLField(blank=True, null=True)
     payload = models.TextField(default='{}')
     user = models.IntegerField()
