871fe87e01e07938842d25916fcf58027109a736
toast38coza <info@38.co.za>
2015-09-30 22:40:19 +0200

    combine some of the tests

--- ifttt/tests/test_helpers.py
+++ ifttt/tests/test_helpers.py
@@ -9,31 +9,16 @@ class TestIFTTTHelpers(TestCase):
     def tearDown(self):
         pass
 
-    def test_correctly_calculate_difference_in_hours(self):
-        enter = "April 29, 2013 at 08:01PM"
-        exit = "April 29, 2013 at 09:01PM"
+    def test_calculate_difference(self):
+        inputs = [
+            ("April 29, 2013 at 08:01PM", "April 29, 2013 at 09:01PM", 1),
+            ("April 29, 2013 at 08:0PM", "April 29, 2013 at 08:31PM", 1),
+            ("April 29, 2013 at 08:0PM", "April 29, 2013 at 08:29PM", 0),
+        ]
 
-        result = IfThisThenThatHelpers.calculate_hours_diff(enter, exit)
-
-        assert result == 1, 'Expect 1 hour to be returned (result = {})' .format (result)
-
-
-    def test_should_round_up_half_hour(self):
-        enter = "April 29, 2013 at 08:0PM"
-        exit = "April 29, 2013 at 08:31PM"
-
-        result = IfThisThenThatHelpers.calculate_hours_diff(enter, exit)
-
-        assert result == 1, 'Expect 1 hour to be returned (result = {})' .format (result)
-
-    def test_should_round_down_under_half_hour(self):
-        enter = "April 29, 2013 at 08:0PM"
-        exit = "April 29, 2013 at 08:29PM"
-
-        result = IfThisThenThatHelpers.calculate_hours_diff(enter, exit)
-
-        assert result == 0, 'Expect 1 hour to be returned (result = {})' .format (result)
-
+        for entered, exited, expected in inputs:
+            result = IfThisThenThatHelpers.calculate_hours_diff(entered, exited)
+            assert result == expected, 'Expect {} to be {})' .format (result, expected)
 
     def test_negative_time_diff(self):
         enter = "April 29, 2013 at 09:01PM"
